a3598a0e5f51c1a13ac2afa36e9e96ea86aaf3eb
Javier Aguila <sjaca10@gmail.com>
2016-01-15 17:08:31 -0600

    Using class based views

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -22,8 +22,13 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # For the first example
     # url(r'^', include('snippets.urls')),
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
+
+    # For the second example
+    # url(r'^snippets/$', views.snippet_list),
+    # url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
+
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
